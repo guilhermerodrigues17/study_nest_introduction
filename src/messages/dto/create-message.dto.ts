bd0b606d630b52078a62e963a5e9d7b846747f84
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-04-28 18:39:12 -0300

    feat: implementados dtos para create e update

---
+++ src/messages/dto/create-message.dto.ts
@@ -0,0 +1,5 @@
+export class CreateMessageDto {
+  readonly content: string;
+  readonly from: string;
+  readonly to: string;
+}
---
+++ src/messages/dto/update-message.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateMessageDto {
+  readonly content?: string;
+  readonly from?: string;
+  readonly to?: string;
+}
