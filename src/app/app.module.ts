51cf90a695e1f29239e1f968715ca49c37eb8b81
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-02 17:13:56 -0300

    build: instalando typeorm e configs do postgres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      database: 'nest_messages',
+      password: 'admin',
+      autoLoadEntities: true,
+      synchronize: true, // NÃO usar em prod
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
