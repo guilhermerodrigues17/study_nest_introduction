148f1aa1a2629ec59d5d715bf83598f633c1a8a3
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-05 15:03:40 -0300

    feat: Adicionado contexto de User ao contexto geral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true, // NÃO usar em prod
     }),
     MessagesModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
