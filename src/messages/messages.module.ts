f15c62c4f0b40a554a0f01553a2b90e039e3b7e9
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-05 17:31:49 -0300

    feat: implementando relação das entidades em todos métodos

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -3,9 +3,10 @@ import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [TypeOrmModule.forFeature([Message]), UserModule],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -4,35 +4,50 @@ import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class MessagesService {
   constructor(
     @InjectRepository(Message)
     private readonly messageRepository: Repository<Message>,
+    private readonly userService: UserService,
   ) {}
 
-  private lastId = 1;
-
-  private messages: Message[] = [
-    {
-      id: 1,
-      content: 'Test content',
-      from: 'John',
-      to: 'John',
-      read: false,
-      date: new Date(),
-    },
-  ];
-
   findAll() {
-    return this.messageRepository.find();
+    return this.messageRepository.find({
+      relations: ['from', 'to'],
+      order: {
+        id: 'desc',
+      },
+      select: {
+        from: {
+          id: true,
+          name: true,
+        },
+        to: {
+          id: true,
+          name: true,
+        },
+      },
+    });
   }
 
   async findOne(id: number) {
     const message = await this.messageRepository.findOne({
       where: {
         id,
+      },
+      relations: ['from', 'to'],
+      select: {
+        from: {
+          id: true,
+          name: true,
+        },
+        to: {
+          id: true,
+          name: true,
+        },
       },
     });
 
@@ -41,38 +56,46 @@ export class MessagesService {
     throw new NotFoundException('Message not found...');
   }
 
-  create(createMessageDto: CreateMessageDto) {
+  async create(createMessageDto: CreateMessageDto) {
+    const { fromId, toId } = createMessageDto;
+
+    const from = await this.userService.findOne(fromId);
+    const to = await this.userService.findOne(toId);
+
     const newMessage = {
-      ...createMessageDto,
+      content: createMessageDto.content,
+      from,
+      to,
       read: false,
       date: new Date(),
     };
-    const message: Message = this.messageRepository.create(newMessage);
 
-    return this.messageRepository.save(message);
+    const message = this.messageRepository.create(newMessage);
+    await this.messageRepository.save(message);
+
+    return {
+      ...message,
+      from: {
+        id: message.from.id,
+      },
+      to: {
+        id: message.to.id,
+      },
+    };
   }
 
   async update(id: number, updateMessageDto: UpdateMessageDto) {
-    const partialUpdateMessageDto = {
-      read: updateMessageDto?.read,
-      content: updateMessageDto?.content,
-    };
+    const message = await this.findOne(id);
 
-    const existentMessage = await this.messageRepository.preload({
-      id,
-      ...partialUpdateMessageDto,
-    });
+    message.content = updateMessageDto?.content ?? message.content;
+    message.read = updateMessageDto?.read ?? message.read;
 
-    if (!existentMessage) throw new NotFoundException('Message not found...');
-
-    return this.messageRepository.save(existentMessage);
+    return this.messageRepository.save(message);
   }
 
   async remove(id: number) {
     const existentMessage = await this.messageRepository.findOneBy({ id });
 
     if (!existentMessage) throw new NotFoundException('Message not found...');
-
-    return this.messageRepository.remove(existentMessage);
   }
 }
