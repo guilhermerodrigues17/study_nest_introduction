756a20215e0df6dba94cc0cd7b4a1ba7feaeb33b
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-05 16:57:51 -0300

    feat: alteração dos tipos de dados para atender a relação entre as entidades

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateMessageDto {
   @IsString()
@@ -7,15 +13,9 @@ export class CreateMessageDto {
   @MaxLength(255)
   readonly content: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  @MaxLength(50)
-  readonly from: string;
+  @IsPositive()
+  readonly fromId: number;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  @MaxLength(50)
-  readonly to: string;
+  @IsPositive()
+  readonly toId: number;
 }
