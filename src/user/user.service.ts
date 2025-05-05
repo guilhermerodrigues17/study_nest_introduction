2cfa757b1d4d6684168314ee7f83998ad06156e5
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-05 15:31:34 -0300

    feat: método create implementado

---
+++ src/user/user.service.ts
@@ -0,0 +1,50 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const userData = {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        passwordHash: createUserDto.password,
+      };
+
+      const newUser = this.userRepository.create(userData);
+      await this.userRepository.save(newUser);
+      return newUser;
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('This email already exists!');
+      }
+
+      throw error;
+    }
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
