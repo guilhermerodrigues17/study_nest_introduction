41b6d5c2a59639f8f53f5fc1f34efec8b253915c
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-13 15:48:58 -0300

    feat: usando interface no provide; implementando protocol pra regex

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  Inject,
   Param,
   Patch,
   Post,
@@ -16,15 +17,32 @@ import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { IsAdminGuard } from 'src/common/guards/is-admin.guard';
+import { RegexProtocol } from 'src/common/regex/regex.protocol';
+import {
+  ONLY_LOWERCASE_LETTERS_REGEX,
+  REMOVE_SPACES_REGEX,
+  SERVER_TEST,
+} from './messages.constant';
 
 // @UseInterceptors(AuthTokenInterceptor)
 @UseGuards(IsAdminGuard)
 @Controller('messages')
 export class MessagesController {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    @Inject(ONLY_LOWERCASE_LETTERS_REGEX)
+    private readonly onlyLowercaseLetters: RegexProtocol,
+    @Inject(REMOVE_SPACES_REGEX)
+    private readonly removeSpaces: RegexProtocol,
+    @Inject(SERVER_TEST)
+    private readonly serverTestString: string,
+  ) {}
 
   @Get()
   async findAll(@Query() paginationDto: PaginationDto) {
+    console.log(this.serverTestString);
+    console.log(this.onlyLowercaseLetters.execute(this.serverTestString));
+    console.log(this.removeSpaces.execute(this.serverTestString));
     const message = await this.messagesService.findAll(paginationDto);
     return message;
   }
--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -4,10 +4,31 @@ import { MessagesService } from './messages.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
 import { UserModule } from 'src/user/user.module';
+import {
+  ONLY_LOWERCASE_LETTERS_REGEX,
+  REMOVE_SPACES_REGEX,
+  SERVER_TEST,
+} from './messages.constant';
+import { OnlyLowercaseLettersRegex } from 'src/common/regex/only-lowercase-letters.regex';
+import { RemoveSpacesRegex } from 'src/common/regex/remove-spaces.regex';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Message]), UserModule],
   controllers: [MessagesController],
-  providers: [MessagesService],
+  providers: [
+    MessagesService,
+    {
+      provide: SERVER_TEST,
+      useValue: 'Server Test: NestJS',
+    },
+    {
+      provide: ONLY_LOWERCASE_LETTERS_REGEX,
+      useClass: OnlyLowercaseLettersRegex,
+    },
+    {
+      provide: REMOVE_SPACES_REGEX,
+      useClass: RemoveSpacesRegex,
+    },
+  ],
 })
 export class MessagesModule {}
