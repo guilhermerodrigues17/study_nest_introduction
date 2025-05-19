37fcd0dbdbff0fa4cdb0ad32b44156f99479413d
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-19 17:37:14 -0300

    feat: endpoint de login verificando se senhas batem

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,22 @@
 import { Global, Module } from '@nestjs/common';
 import { HashingProtocolService } from './hashing/hashing-protocol.service';
 import { BcryptService } from './hashing/bcrypt.service';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Global()
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [
     {
       provide: HashingProtocolService,
       useClass: BcryptService,
     },
+    AuthService,
   ],
   exports: [HashingProtocolService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingProtocolService } from './hashing/hashing-protocol.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingProtocolService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.userRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (!user) throw new UnauthorizedException('Non-existing user!');
+
+    const passwordIsValid: boolean = await this.hashingService.compare(
+      loginDto.password,
+      user.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException("Passwords don't match!");
+    }
+
+    return {
+      message: 'User successfully logged in',
+    };
+  }
+}
