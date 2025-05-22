b0691f7d2d4baf74a9733242ddf0ef273001700a
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-22 17:44:26 -0300

    feat: implementada regra do user só poder atualizar ou deletar seus próprios dados

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,8 +15,9 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { TimingConnectionInterceptor } from 'src/common/interceptors/timing-connection.interceptor';
 import { UrlParam } from 'src/common/params/url-param.decorator';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
+import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
 
-@UseGuards(AuthTokenGuard)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -27,24 +28,35 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Get()
   @UseInterceptors(TimingConnectionInterceptor)
   findAll() {
     return this.userService.findAll();
   }
 
+  @UseGuards(AuthTokenGuard)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.userService.findOne(id);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto);
+  update(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+    @TokenPayloadParam() tokenPayloadDto: TokenPayloadDto,
+  ) {
+    return this.userService.update(id, updateUserDto, tokenPayloadDto);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.userService.remove(id);
+  remove(
+    @Param('id') id: number,
+    @TokenPayloadParam() tokenPayloadDto: TokenPayloadDto,
+  ) {
+    return this.userService.remove(id, tokenPayloadDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import {
   ConflictException,
+  ForbiddenException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -9,6 +10,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { HashingProtocolService } from 'src/auth/hashing/hashing-protocol.service';
+import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
 
 @Injectable()
 export class UserService {
@@ -57,7 +59,11 @@ export class UserService {
     return userFound;
   }
 
-  async update(id: number, updateUserDto: UpdateUserDto) {
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+    tokenPayloadDto: TokenPayloadDto,
+  ) {
     const userData = {
       name: updateUserDto?.name,
     };
@@ -79,14 +85,22 @@ export class UserService {
       throw new NotFoundException('User not found...');
     }
 
+    if (userUpdated.id !== tokenPayloadDto.sub) {
+      throw new ForbiddenException('You can only change your own data...');
+    }
+
     return this.userRepository.save(userUpdated);
   }
 
-  async remove(id: number) {
+  async remove(id: number, tokenPayloadDto: TokenPayloadDto) {
     const userFound = await this.userRepository.findOneBy({ id });
 
     if (!userFound) {
       throw new NotFoundException('User not found...');
+    }
+
+    if (userFound.id !== tokenPayloadDto.sub) {
+      throw new ForbiddenException('You can only change your own data...');
     }
 
     return this.userRepository.remove(userFound);
