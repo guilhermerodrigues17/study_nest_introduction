3a7b7823f3faba4a50b300d9a0237107b728580d
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-05-28 16:55:10 -0300

    feat: implementado endpoint para upload de imagens

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,6 +8,10 @@ import {
   Delete,
   UseInterceptors,
   UseGuards,
+  UploadedFile,
+  ParseFilePipeBuilder,
+  HttpStatus,
+  BadRequestException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -17,6 +21,9 @@ import { UrlParam } from 'src/common/params/url-param.decorator';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path';
+import * as fs from 'fs/promises';
 
 @Controller('users')
 export class UserController {
@@ -58,5 +65,49 @@ export class UserController {
     @TokenPayloadParam() tokenPayloadDto: TokenPayloadDto,
   ) {
     return this.userService.remove(id, tokenPayloadDto);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload-picture')
+  async uploadPicture(
+    @TokenPayloadParam() tokenPayloadDto: TokenPayloadDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /jpeg|jpg|png/g,
+        })
+        .addMaxSizeValidator({
+          maxSize: 10 * (1024 * 1024),
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    if (!file) {
+      throw new BadRequestException('File is too small');
+    }
+
+    if (file.size < 1024) {
+      throw new BadRequestException('File is too small');
+    }
+
+    const result: string[] = [];
+
+    const fileExtension = path
+      .extname(file.originalname)
+      .toLowerCase()
+      .substring(1);
+
+    const fileName = `${tokenPayloadDto.sub}.${fileExtension}`;
+    const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
+
+    await fs.writeFile(fileFullPath, file.buffer);
+
+    result.push(fileFullPath);
+
+    return result;
   }
 }
