2bbf19e9fdd104a94196867b6f97ba9c9f01be47
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-06-04 17:39:57 -0300

    test(userService): testes do método create

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,6 +4,8 @@ import { User } from './entities/user.entity';
 import { HashingProtocolService } from 'src/auth/hashing/hashing-protocol.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ConflictException } from '@nestjs/common';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -16,11 +18,16 @@ describe('UserService', () => {
         UserService,
         {
           provide: getRepositoryToken(User),
-          useValue: {},
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
         },
         {
           provide: HashingProtocolService,
-          useValue: {},
+          useValue: {
+            hash: jest.fn(),
+          },
         },
       ],
     }).compile();
@@ -30,7 +37,58 @@ describe('UserService', () => {
     hashingService = module.get(HashingProtocolService);
   });
 
-  test('userService should be defined', () => {
+  it('userService should be defined', () => {
     expect(userService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'email@email.com',
+        name: 'name',
+        password: 'password',
+      };
+
+      const passwordHash = 'passwordHash';
+
+      const userData = {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        passwordHash,
+      };
+
+      const newUser = {
+        id: 1,
+        ...userData,
+      };
+
+      jest.spyOn(hashingService, 'hash').mockResolvedValue(passwordHash);
+      jest.spyOn(userRepository, 'create').mockReturnValue(newUser as any);
+
+      const result = await userService.create(createUserDto);
+
+      expect(hashingService.hash).toHaveBeenCalledWith(createUserDto.password);
+      expect(userRepository.create).toHaveBeenCalledWith(userData);
+      expect(userRepository.save).toHaveBeenCalledWith(newUser);
+      expect(result).toEqual(newUser);
+    });
+
+    it('should throw ConflictException when email already exists in db', async () => {
+      jest.spyOn(userRepository, 'save').mockRejectedValue({ code: '23505' });
+
+      await expect(userService.create({} as any)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+
+    it('should throw a generic Error when ConflictException is not thrown', async () => {
+      jest
+        .spyOn(userRepository, 'save')
+        .mockRejectedValue(new Error('generic'));
+
+      await expect(userService.create({} as any)).rejects.toThrow(
+        new Error('generic'),
+      );
+    });
   });
 });
